cd85dfcd3f40c49eeeba8df3d65ef59cbd3e3604
Thapyay Aung <thapyayaung.tpa@gmail.com>
2019-06-19 17:18:21 +0630

    Added
    Testing the socket.io

---
+++ youtubeAPI-iframe-demo/bk/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const port = process.env.PORT || 3000;
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+http.listen(port, function() {
+    console.log(`Listening on port : ${port}`);
+});
+
+io.sockets.on('connection', function(socket) {
+    console.log('A new client connected');
+});
